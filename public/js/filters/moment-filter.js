279b6da4b050f148641ed1c0c2ea0831699389b2
rithms <tcaldwel@nmsu.edu>
2015-09-03 23:14:10 -0400

    Format date using moment.js filter

---
+++ public/js/filters/moment-filter.js
@@ -0,0 +1,7 @@
+angular.module('ItemSetApp').filter('moment', function () {
+  return function (input, momentFn /*, param1, param2, ...param n */) {
+    var args = Array.prototype.slice.call(arguments, 2),
+        momentObj = moment(input);
+    return momentObj[momentFn].apply(momentObj, args);
+  };
+});
